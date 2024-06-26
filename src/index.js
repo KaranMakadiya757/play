3e047999b4d279198f580c13ff6404e9f8361c25
KaranMakadiya757 <karanmakadiya87@gmail.com>
2024-06-26 18:25:42 +0530

    created the utils for asynchandle, apierror and apiresponse. added user and video mode;

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import connectDB from "./db/index.js";
+import { app } from "./app.js"
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000,() => { console.log(`server running on ${process.env.PORT}`)})
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error !!! ", err);
+    })
