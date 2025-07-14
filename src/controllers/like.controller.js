1e7d31c34dcebc7d9559de20aba9eb23d1fad31d
KaranMakadiya757 <karanmakadiya87@gmail.com>
2025-07-14 16:35:53 +0530

    added like module logic

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,24 +4,188 @@ import { ApiError } from "../utils/apiError.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import { Like } from '../models/like.model.js'
 
+const getLikedVideos = asyncHandler(async (req, res) => {
+
+    // Get liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: req.user._id
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        _id: 1,
+                                        avatar: 1,
+                                        username: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: { $first: "$owner" },
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                video: { $first: "$video"}
+            }
+        },
+        {
+            $project: {
+                video: 1
+            }
+        }
+    ])
+
+    // Throw Error is we get error while fetching liked videos
+    if (!likedVideos) {
+        throw new ApiError(500, "Internal Server Error !!!");
+    }
+
+    // Return respone
+    return res
+        .status(200)
+        .json(new ApiResponse(200, likedVideos, "Videos Fetched SuccessFully"));
+})
+
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: toggle like on video
+
+    // Create a Like object
+    const likeobj = {
+        likedBy: req.user._id,
+        video: req.video._id
+    }
+
+    // Check Wheather the like is added or not
+    const like = await Like.findOne(likeobj);
+
+    // Toggle the like
+    if (!like) {
+
+        // Add Like
+        const newLike = await Like.create(likeobj);
+
+        // Throw error 
+        if (!newLike) throw new ApiError(500, "Something went wrong while adding like");
+
+        // Return response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Added like to video"))
+
+    } else {
+
+        // remove the subscription
+        const removedLike = await Like.deleteOne(likeobj);
+
+        // Throw Error
+        if (!removedLike) throw new ApiError(500, "Something went wrong while removing like");
+
+        // Return Response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Like removed from video"));
+    }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const { commentId } = req.params
-    //TODO: toggle like on comment
+
+    // Create a Like object
+    const likeobj = {
+        likedBy: req.user._id,
+        comment: req.comment._id
+    }
+
+    // Check Wheather the like is added or not
+    const like = await Like.findOne(likeobj);
+
+    // Toggle the like
+    if (!like) {
+
+        // Add Like
+        const newLike = await Like.create(likeobj);
+
+        // Throw error 
+        if (!newLike) throw new ApiError(500, "Something went wrong while adding like");
+
+        // Return response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Added like to comment"))
+
+    } else {
+
+        // remove the subscription
+        const removedLike = await Like.deleteOne(likeobj);
+
+        // Throw Error
+        if (!removedLike) throw new ApiError(500, "Something went wrong while removing like");
+
+        // Return Response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Like removed from comment"));
+    }
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const { tweetId } = req.params
-    //TODO: toggle like on tweet
-}
-)
 
-const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    // Create a Like object
+    const likeobj = {
+        likedBy: req.user._id,
+        tweet: req.tweet._id
+    }
+
+    // Check Wheather the like is added or not
+    const like = await Like.findOne(likeobj);
+
+    // Toggle the like
+    if (!like) {
+
+        // Add Like
+        const newLike = await Like.create(likeobj);
+
+        // Throw error 
+        if (!newLike) throw new ApiError(500, "Something went wrong while adding like");
+
+        // Return response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Added like to tweet"))
+
+    } else {
+
+        // remove the subscription
+        const removedLike = await Like.deleteOne(likeobj);
+
+        // Throw Error
+        if (!removedLike) throw new ApiError(500, "Something went wrong while removing like");
+
+        // Return Response
+        return res
+            .status(201)
+            .json(new ApiResponse(200, {}, "Like removed from tweet"));
+    }
 })
 
 export {
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,18 +1,30 @@
 import { Router } from 'express';
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import verifyId from "../middlewares/verifyId.middleware.js";
+
 import {
     getLikedVideos,
     toggleCommentLike,
     toggleVideoLike,
     toggleTweetLike,
 } from "../controllers/like.controller.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
 
+// Router instance
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
-router.route("/toggle/c/:commentId").post(toggleCommentLike);
-router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+// Secured Routes
+router.use(verifyJWT);
+
+// Get all liked videos
 router.route("/videos").get(getLikedVideos);
+
+// Toggle Video likes
+router.route("/toggle/v/:videoId").post(verifyId, toggleVideoLike);
+
+// Toggle Comment likes
+router.route("/toggle/c/:commentId").post(verifyId, toggleCommentLike);
+
+// Toggle Tweet likes
+router.route("/toggle/t/:tweetId").post(verifyId, toggleTweetLike);
 
 export default router
