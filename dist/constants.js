63fae11e2958fcdf8748dfa2752c1ea3eddc0dfc
KaranMakadiya757 <karanmakadiya87@gmail.com>
2026-01-06 12:53:12 +0530

    removed dist from git

--- dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cookieOption = exports.DB_NAME = void 0;
-exports.DB_NAME = "PlayTube";
-exports.cookieOption = {
-    httpOnly: true,
-    secure: true,
-};
-//# sourceMappingURL=constants.js.map
